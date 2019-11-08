15a6a09ef954a220a3784d36ce217169d8fbd4a2
Salazar-Prime <aggarw82@purdue.edu>
2019-11-08 14:56:24 -0500

    Create SAT.py

---
+++ SAT.py
@@ -0,0 +1,13 @@
+# define symbols
+sym_AND = '&'
+
+# input expression
+in_exp = "( a |b|( ~c))&((~a )|c)&((~b)|c )&((~d)|(~ e)|(~f ))&(( d )|(e)|( ~ f) )&((  d)|(~  e) |( f))&((~d)|(e)|(f))&((~e)|(~f)|(~g))&((e)|(f)|(~g))&((e)|(~f)|(g))&((~e)|(f)|(g))&g&(~g)"
+in_exp = re.sub(r'\s+', '', in_exp) # removing white spaces
+
+# seperating into list of clauses
+cl_list = sorted(in_exp.split(sym_AND), key=len)
+
+
+# list of variables
+vr_list = sorted(set(filter(None,re.split(r'[|&)(~]+',in_exp))))
